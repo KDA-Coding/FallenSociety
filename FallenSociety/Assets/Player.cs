82b770045623ec0ea6c0bcee4c1435dce2c4a42b
KDA-Coding <59184838+KDA-Coding@users.noreply.github.com>
2022-03-05 17:36:15 -0500

    Edited Player Script.
    Included functionality for player movement and sprite reorientation.

--- FallenSociety/Assets/Player.cs
+++ FallenSociety/Assets/Player.cs
@@ -2,14 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
 
+    private Vector3 moveDelta;
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        //Reset moveDelta
+        moveDelta = new Vector3(moveX, moveY, 0);
+
+        //Switch Sprite facing based on move direction (Right or Left)
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //Move the player sprite
+        transform.Translate(moveDelta * Time.deltaTime);
+
+        
+        //Debugs for Movement floats
+        /*
+        Debug.Log(moveX);
+        Debug.Log(moveY); */
     }
 }
